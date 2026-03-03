80e6c2590933c0e3cf466cb64460a997f339390e
Bjuahdz <bryanjuarez91286@gmail.com>
2026-03-03 16:45:51 -0500

    Wrap Stack component in AuthProvider for authentication context in RootLayout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,7 +1,12 @@
+import { AuthProvider } from '@/context/AuthContext';
 import { Stack } from 'expo-router';
 
 export const unstable_settings = { anchor: '(tabs)' };
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </AuthProvider>
+  );
 }
